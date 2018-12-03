0d296a6ce69d97eff835f9cb76c87eaa7f455499
henrip11 <44254650+henrip11@users.noreply.github.com>
2018-12-03 15:55:50 +0900

    Add files via upload

--- Glove_dataloader.py
+++ Glove_dataloader.py
@@ -1,13 +1,12 @@
 
 import numpy as np
-def loadGloveModel():
-    print( "Loading Glove Model")
-    f = open("/Users/henriparssinen/desktop/Introduction to AI/keio2018aia/course/general/project1/data/wv_50d.txt",'r')
-    model = {}
-    for line in f:
-        splitLine = line.split()
-        word = splitLine[0]
-        embedding = np.array([float(val) for val in splitLine[1:]])
-        model[word] = embedding
-    print ("Done.",len(model)," words loaded!")
-    return model
+f = open("/Users/henriparssinen/desktop/Introduction to AI/keio2018aia/course/general/project1/data/wv_50d.txt",'r')
+model = {}
+for line in f:
+    splitLine = line.split()
+    word = splitLine[0]
+    embedding = np.array([float(val) for val in splitLine[1:]])
+    model[word] = embedding
+print ("Done.",len(model)," words loaded!")
+
+print (model['the'])
