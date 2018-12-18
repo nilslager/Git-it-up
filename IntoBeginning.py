b35a0a267696b8ac93efb6fee0e210ecbbe4fa01
OlaHjeltnes <45550460+OlaHjeltnes@users.noreply.github.com>
2018-12-18 15:18:58 +0900

    Add files via upload

--- IntoBeginning.py
+++ IntoBeginning.py
@@ -6,7 +6,7 @@ Created on Sat Dec  8 14:38:33 2018
 """
 
 import numpy as np
-f = open("C:/Users/Ola/Documents/School/Keio/keio2018aia/course/general/project1/data/wv_50d.txt",'r')
+f = open('/Users/Ola/Documents/School/Keio/AI/wv_50d.txt','r')
 model = {}
 for line in f:
     splitLine = line.split()
@@ -14,7 +14,7 @@ for line in f:
     embedding = np.array([float(val) for val in splitLine[1:]])
     model[word] = embedding
 
-path='/Users/Ola/Documents/School/Keio/keio2018aia/course/general/project1/data/senti_binary.train'
+path='/Users/Ola/Documents/School/Keio/AI/senti_binary.train'
 revfile=open(path)
 
 def revfile_clean():
@@ -26,7 +26,7 @@ def revfile_clean():
     return clean_lines
 
 ready_rev=revfile_clean() 
-ready_rev_edit=ready_rev      
+ready_rev_edit=revfile_clean()      
 
 #1 replace words with vectors
 for i in range(len(ready_rev)):
@@ -34,13 +34,19 @@ for i in range(len(ready_rev)):
         if j is not (len(ready_rev[i])-1):
             if ready_rev[i][j+1] is "'s":
                 ready_rev[i][j]=ready_rev[i][j]+ready_rev[i][j+1]
+                #Delete j+1 word
                 j=j-1
             try:
-                ready_rev_edit[i][j]=model[ready_rev[i][j]]
+                ready_rev_edit[i][j]=np.sum(model[ready_rev[i][j]])
             except KeyError:
                 model.update({ready_rev[i][j]:np.zeros(50)})
                 ready_rev_edit[i][j]=model[ready_rev[i][j]]
-                    
+                
+            sum(np.sum(model[ready_rev[i][j]]).values(-j))
+            np.sum(model(ready_rev_edit[1][1]))
+            sum(model(ready_rev[1][1])).value())
+        sum(model['character'].value())
+
                     
             
             
